1556d9d685a4b1eb384f90fa00e58bb2d88fc3ea
youssef <youssefahmedabdelkader25@gmail.com>
2024-12-20 16:18:50 +0200

    refactor: remove timestamps from the code and make the betty for the code

--- models/amenity.js
+++ models/amenity.js
@@ -1,38 +1,33 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const amenitySchema = new mongoose.Schema(
-  {
-    id: {
-      type: String,
-      default: uuidv4,
-      unique: true,
-      immutable: true,
-    },
-    name: {
-      type: String,
-      required: true,
-      maxlength: 100,
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-    updated_at: {
-      type: Date,
-      default: Date.now,
-    },
-    hotel_amenities: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Hotel",
-      },
-    ],
+const amenitySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+    immutable: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  name: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+  hotel_amenities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+    },
+  ],
+});
 
 const Amenity = mongoose.model("Amenity", amenitySchema);
 module.exports = Amenity;
--- models/review.js
+++ models/review.js
@@ -2,48 +2,43 @@ const mongoose = require("mongoose");
 
 const { v4: uuidv4 } = require("uuid");
 
-const reviewSchema = new mongoose.Schema(
-  {
-    id: {
-      type: String,
-      default: uuidv4,
-      unique: true,
-      immutable: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    description: {
-      type: String,
-      required: true,
-      maxlength: 200,
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-    updated_at: {
-      type: Date,
-      default: Date.now,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    hotel: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Hotel",
-      required: true,
-    },
+const reviewSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+    immutable: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 200,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  hotel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hotel",
+    required: true,
+  },
+});
 
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
